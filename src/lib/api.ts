35925f882355ea886cef31ad35a093a9afd6f83b
iam-harshprajapat <s.harshprajapat@gmail.com>
2025-11-11 02:13:51 +0530

    posts fetching from api

---
+++ src/lib/api.ts
@@ -0,0 +1,26 @@
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export async function getAllPosts() {
+  const token = localStorage.getItem("token");
+  try {
+    const res = await fetch(`${API_BASE_URL}/api/post/get-all-posts`, {
+      method: "GET",
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+        "Content-Type": "application/json"
+      },
+      cache: "no-store",
+
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch posts: ${res.statusText}`);
+      console.log(res);
+    }
+
+    return res.json();
+  } catch (err) {
+    console.error("Error fetching posts:", err);
+    throw err;
+  }
+}
